07764d12de8fc0b63001b021510b960380ee6db7
Hugo Pereira <hugoper_cs@yahoo.com>
2026-09-08 14:31:50 -0300

    feat(server): create HTTP server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log('Server is running...');
+import * as http from 'http';
+
+// Criando um servidor HTTP
+const server = http.createServer(
+  (request: http.IncomingMessage, reponse: http.ServerResponse) => {}
+);
+
+const port = process.env.PORT;
+
+// Iniciando o servidor na porta 3333
+server.listen(port, () => {
+  console.log(`Servidor iniciado na porta ${port}.`)
+})
